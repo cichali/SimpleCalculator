9e3767dd71715b0f66eef031390b8c00779e42af
cichali <cichyakms@gmail.com>
2020-07-16 20:02:48 +0200

    working Simple Calculator

--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -10,6 +10,97 @@ namespace SimpleCalculator
     {
         static void Main(string[] args)
         {
+            float firstNumber, secondNumber, result = 0;
+            bool checkNumber;
+            string enteredOperator;
+
+            string[] mathOperator = new string[4] {"+", "-", "*", "/"};
+
+            // Welcome text
+            Console.WriteLine("Welcome to: Simple Console Calculator!");
+            Console.WriteLine("");
+            Console.WriteLine("Available operations: addition, subtraction, multiplication, division.");
+            Console.WriteLine("");
+            Console.WriteLine("Start by entering number!");
+            Console.WriteLine("");
+
+            // Main loop
+            while (true)
+            {
+                // Reciving first number from user
+                do
+                {
+                    Console.WriteLine("------");
+                    Console.Write("First number: ");
+                    checkNumber = float.TryParse(Console.ReadLine(), out firstNumber);
+
+                    if (!checkNumber)
+                    {
+                        Console.WriteLine("!!!");
+                        Console.WriteLine("error, enter a number!");
+                        Console.WriteLine("!!!");
+                    }
+                }
+                while (!checkNumber);
+
+                // Reciving math operator from user
+                do
+                {
+                    Console.Write("Math operator: ");
+                    enteredOperator = Console.ReadLine();
+
+                    if (!mathOperator.Contains(enteredOperator))
+                    {
+                        Console.WriteLine("!!!");
+                        Console.WriteLine("error, enter correct math operator(+,-,*,/)!");
+                        Console.WriteLine("!!!");
+                    }
+                }
+                while (!mathOperator.Contains(enteredOperator));
+
+                // Reciving second number from user
+                do
+                {
+                    Console.Write("Second number: ");
+                    checkNumber = float.TryParse(Console.ReadLine(), out secondNumber);
+
+                    if (!checkNumber)
+                    {
+                        Console.WriteLine("!!!");
+                        Console.WriteLine("error, enter a number!");
+                        Console.WriteLine("!!!");
+                    }
+                }
+                while (!checkNumber);
+
+                // Performing math operations
+                if (enteredOperator == mathOperator[0])
+                {
+                    result = firstNumber + secondNumber;
+                }
+
+                else if (enteredOperator == mathOperator[1])
+                {
+                    result = firstNumber - secondNumber;
+                }
+
+                else if (enteredOperator == mathOperator[2])
+                {
+                    result = firstNumber * secondNumber;
+                }
+
+                else if (enteredOperator == mathOperator[3])
+                {
+                    result = firstNumber / secondNumber;
+                }
+
+                // Printing result
+                Console.WriteLine("");
+                Console.WriteLine($"Result: {firstNumber} {enteredOperator} {secondNumber} = {result}");
+                Console.WriteLine("");
+
+            }
+
         }
     }
 }
